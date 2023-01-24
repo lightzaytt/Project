91a6bf8694191c530166fca0b194810bf97b7cfe
lightzaytt <v_luck.say6@yahoo.com.tw>
2023-01-24 23:37:39 +0800

    host

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.use(
   favouriteRoute
 );
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080.");
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
 });
